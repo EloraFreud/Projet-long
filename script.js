24fcda41cd262fcf1d71011f662ef257dee87390
Elora <elora.freud@hotmail.fr>
2023-06-17 00:58:28 +0200

    lien entre les pages

--- script.js
+++ script.js
@@ -69,7 +69,100 @@ function updateActivityNav(activeActivityLink) {
       })
 }
 
+// nav community + home
+const navCommunity = document.querySelectorAll("#nav-community");
+const navHome= document.querySelectorAll("#nav-home");
+const communityPage = document.querySelector("#community-page");
+const homePage = document.querySelector("#home-page");
+
+navCommunity.forEach((link) => {
+  console.log("le truc que je veux tester :");
+  console.log(link);
+  link.addEventListener("click", () => {
+
+    communityPage.classList.add("visible");
+    communityPage.classList.remove("none");
+    
+    homePage.classList.add("none");
+    homePage.classList.remove("visible");
+  });
+});
+
+navHome.forEach((link) => {
+  link.addEventListener("click", () => {
+    
+    homePage.classList.add("visible");
+    homePage.classList.remove("none");
+    
+    communityPage.classList.add("none");
+    communityPage.classList.remove("visible");
+  });
+});
 
 
+// nav subject to quiz1 to quiz2 to activity
+const subject = document.querySelectorAll("#subject");
+const quiz1 = document.querySelector("#quiz-1");
+const quiz2 = document.querySelector("#quiz-2");
+const subjectPage = document.querySelector("#subject-page");
+const quizCloseButton = document.querySelectorAll("#quiz-close");
+const quizButtonSuivant = document.querySelectorAll("#quiz-button-suivant");
+const quizButtonCommencer = document.querySelectorAll("#quiz-button-commencer");
+const sectionActivity = document.querySelector("#section-activity");
 
+subject.forEach((link) => {
+  link.addEventListener("click", () => {
 
+    quiz1.classList.add("visible");
+    quiz1.classList.remove("none");
+    
+    subjectPage.classList.add("none");
+    subjectPage.classList.remove("visible");
+  });
+});
+
+quizCloseButton.forEach((link) => {
+  link.addEventListener("click", () => {
+
+    quiz1.classList.add("none");
+    quiz1.classList.remove("visible");
+    
+    subjectPage.classList.add("visible");
+    subjectPage.classList.remove("none");
+  });
+});
+
+quizButtonSuivant.forEach((link) => {
+  link.addEventListener("click", () => {
+
+    quiz1.classList.add("none");
+    quiz1.classList.remove("visible");
+    
+    quiz2.classList.add("visible");
+    quiz2.classList.remove("none");
+  });
+});
+
+quizButtonCommencer.forEach((link) => {
+  link.addEventListener("click", () => {
+
+    quiz2.classList.add("none");
+    quiz2.classList.remove("visible");
+    
+    sectionActivity.classList.add("visible");
+    sectionActivity.classList.remove("none");
+  });
+});
+
+//close activity
+const closeActivity = document.querySelectorAll("#activity-close");
+closeActivity.forEach((link) => {
+  link.addEventListener("click", () => {
+
+    sectionActivity.classList.add("none");
+    sectionActivity.classList.remove("visible");
+    
+    subjectPage.classList.add("visible");
+    subjectPage.classList.remove("none");
+  });
+});
